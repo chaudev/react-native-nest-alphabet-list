c00f32f1807bf8e55d0f6170dbd5a3398bd5d153
Châu Đệ Nhất <soc900k@gmail.com>
2023-01-04 22:07:23 +0700

    first commit

---
+++ built/Toast.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true }); 
+let toast = null;
+exports.default = {
+    show(content) { 
+        toast =  {}
+    }
+};
+//# sourceMappingURL=Toast.js.map
